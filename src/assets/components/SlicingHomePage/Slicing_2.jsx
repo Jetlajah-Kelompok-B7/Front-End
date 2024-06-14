28e7f2a9bc1e436cef5794a50b3a669d2810a61a
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-06-14 17:16:49 +0700

    menambah contaner di home page dan memperbaiki navbar

--- src/assets/components/SlicingHomePage/Slicing_2.jsx
+++ src/assets/components/SlicingHomePage/Slicing_2.jsx
@@ -13,86 +13,88 @@ export default function Slicing_2() {
   };
 
   return (
-    <div className="mx-[236px] mt-[87px]">
-      <div>
-        <h1 className=" text-[40px] font-bold mb-4">
-          Destinasi <span className="text-[#FE5D02]">Favorit</span>
-        </h1>
-        <div className=" mx-5">
-          <div className=" flex gap-4 text-white">
-            <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
-              <img
-                src="/images/search.png"
-                alt=""
-                className="h-[20px] w-[20px]"
-              />
-              <p>Semua</p>
-            </button>
-            <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
-              <img
-                src="/images/search.png"
-                alt=""
-                className="h-[20px] w-[20px]"
-              />
-              <p>Semua</p>
-            </button>
-            <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
-              <img
-                src="/images/search.png"
-                alt=""
-                className="h-[20px] w-[20px]"
-              />
-              <p>Semua</p>
-            </button>
-            <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
-              <img
-                src="/images/search.png"
-                alt=""
-                className="h-[20px] w-[20px]"
-              />
-              <p>Semua</p>
-            </button>
-            <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
-              <img
-                src="/images/search.png"
-                alt=""
-                className="h-[20px] w-[20px]"
-              />
-              <p>Semua</p>
-            </button>
-            <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
-              <img
-                src="/images/search.png"
-                alt=""
-                className="h-[20px] w-[20px]"
-              />
-              <p>Semua</p>
+    <div className="container mx-auto">
+      <div className="mx-[236px] mt-[87px]">
+        <div>
+          <h1 className=" text-[40px] font-bold mb-4">
+            Destinasi <span className="text-[#FE5D02]">Favorit</span>
+          </h1>
+          <div className=" mx-5">
+            <div className=" flex gap-4 text-white">
+              <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
+                <img
+                  src="/images/search.png"
+                  alt=""
+                  className="h-[20px] w-[20px]"
+                />
+                <p>Semua</p>
+              </button>
+              <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
+                <img
+                  src="/images/search.png"
+                  alt=""
+                  className="h-[20px] w-[20px]"
+                />
+                <p>Semua</p>
+              </button>
+              <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
+                <img
+                  src="/images/search.png"
+                  alt=""
+                  className="h-[20px] w-[20px]"
+                />
+                <p>Semua</p>
+              </button>
+              <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
+                <img
+                  src="/images/search.png"
+                  alt=""
+                  className="h-[20px] w-[20px]"
+                />
+                <p>Semua</p>
+              </button>
+              <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
+                <img
+                  src="/images/search.png"
+                  alt=""
+                  className="h-[20px] w-[20px]"
+                />
+                <p>Semua</p>
+              </button>
+              <button className="flex gap-2 bg-[#176B87] rounded-2xl py-[14px]  px-6 items-center">
+                <img
+                  src="/images/search.png"
+                  alt=""
+                  className="h-[20px] w-[20px]"
+                />
+                <p>Semua</p>
+              </button>
+            </div>
+            {/* Gambar */}
+            <div className="grid grid-cols-5 gap-5 my-5">
+              <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
+              <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
+              <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
+              <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
+              <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
+            </div>
+          </div>
+          <div className="flex gap-2   justify-center text-white">
+            <button className="bg-[#176B87] py-[14px]  px-6 items rounded-2xl">
+              More
             </button>
           </div>
-          {/* Gambar */}
-          <div className="grid grid-cols-5 gap-5 my-5">
-            <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
-            <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
-            <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
-            <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
-            <div className="w-full h-[200px] bg-blue-300 rounded-md"></div>
+        </div>
+        <div className="mt-20">
+          <h1 className=" text-[40px] font-bold mb-4">
+            Promo Menarik <span className="text-[#FE5D02]">Tiket Pesawat</span>
+          </h1>
+          <div className="slider-container px-9 custom-slick">
+            <Slider {...sliderSettings}>
+              <div className="w-full h-[200px] bg-blue-300 rounded-2xl"></div>
+              <div className="w-full h-[200px] bg-blue-300 rounded-2xl"></div>
+            </Slider>
           </div>
-        </div>
-        <div className="flex gap-2   justify-center text-white">
-          <button className="bg-[#176B87] py-[14px]  px-6 items rounded-2xl">
-            More
-          </button>
-        </div>
-      </div>
-      <div className="mt-20">
-        <h1 className=" text-[40px] font-bold mb-4">
-          Promo Menarik <span className="text-[#FE5D02]">Tiket Pesawat</span>
-        </h1>
-        <div className="slider-container px-9 custom-slick">
-          <Slider {...sliderSettings}>
-            <div className="w-full h-[200px] bg-blue-300 rounded-2xl"></div>
-            <div className="w-full h-[200px] bg-blue-300 rounded-2xl"></div>
-          </Slider>
         </div>
       </div>
     </div>
--- src/assets/components/SlicingHomePage/Slicing_3.jsx
+++ src/assets/components/SlicingHomePage/Slicing_3.jsx
@@ -2,44 +2,46 @@ import React from "react";
 
 export default function Slicing_3() {
   return (
-    <div className=" mt-40 mx-[236px]">
-      <div>
-        <h1 className=" text-[40px] font-bold mb-4">
-          About<span className="text-[#FE5D02]">Jetlajah.In</span>
-        </h1>
-        <p className="ml-6 mt-[27px] mb-2 text-[20px]">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently.
-        </p>
-        <div className="flex gap-2   justify-center text-white">
-          <button className="bg-[#176B87] py-[14px]  px-6 items rounded-2xl">
-            More
-          </button>
-        </div>
-      </div>
-      <div>
-        <h1 className=" text-[40px] font-bold mb-4 mt-16 ">
-          Partner <span className="text-[#FE5D02]">Maskapai</span>
-        </h1>
-        <div className="flex gap-16 ml-6  ">
-          <div className="flex-1 flex flex-col justify-between text-[20px] ">
-            <p className=" text-[#8A8A8A] ">
-              Partner Maskapai Penerbangan di Dalam & Luar Negeri
-            </p>
-            <p>
-              Kami bekerja sama dengan berbagai maskapai penerbangan di seluruh
-              dunia untuk menerbangkan Anda ke mana pun Anda inginkan!
-            </p>
+    <div className="container mx-auto">
+      <div className=" mt-40 mx-[236px]">
+        <div>
+          <h1 className=" text-[40px] font-bold mb-4">
+            About<span className="text-[#FE5D02]">Jetlajah.In</span>
+          </h1>
+          <p className="ml-6 mt-[27px] mb-2 text-[20px]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently.
+          </p>
+          <div className="flex gap-2   justify-center text-white">
+            <button className="bg-[#176B87] py-[14px]  px-6 items rounded-2xl">
+              More
+            </button>
           </div>
+        </div>
+        <div>
+          <h1 className=" text-[40px] font-bold mb-4 mt-16 ">
+            Partner <span className="text-[#FE5D02]">Maskapai</span>
+          </h1>
+          <div className="flex gap-16 ml-6  ">
+            <div className="flex-1 flex flex-col justify-between text-[20px] ">
+              <p className=" text-[#8A8A8A] ">
+                Partner Maskapai Penerbangan di Dalam & Luar Negeri
+              </p>
+              <p>
+                Kami bekerja sama dengan berbagai maskapai penerbangan di
+                seluruh dunia untuk menerbangkan Anda ke mana pun Anda inginkan!
+              </p>
+            </div>
 
-          <div className="flex-1">
-            <img src="/images/partner.png" alt="" />
+            <div className="flex-1">
+              <img src="/images/partner.png" alt="" />
+            </div>
           </div>
         </div>
       </div>
