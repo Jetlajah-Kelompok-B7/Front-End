b39f066562c6a7ecb9821f814c3901a7614f7da5
rizqi-r <rizzzqi.r@gmail.com>
2024-06-21 20:04:46 +0700

    style: styling vite config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://jetlajahin.up.railway.app/",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://jetlajahin.up.railway.app",
+                changeOrigin: true,
+                secure: false
+            }
+        },
+    },
 });
