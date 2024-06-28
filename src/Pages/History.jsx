5671fb1359f2ac630af5b0bba48ac189fdd41983
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-06-29 03:13:12 +0700

    Update History.jsx

--- src/Pages/History.jsx
+++ src/Pages/History.jsx
@@ -13,7 +13,7 @@ import SearchHistoryModal from "../assets/components/Modal/SearchHistoryModal";
 import { format, parseISO } from "date-fns";
 import id from "date-fns/locale/id";
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function History() {
   const [modal, setModal] = useState("");
@@ -23,6 +23,7 @@ export default function History() {
   const [itemsPerPage] = useState(10);
   const [dataTiket, setDataTiket] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   //pengaman agar jika user belum login
   const Condition = useSelector((state) => {
