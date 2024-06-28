5ea0361262425dbfc085ca5e47dd1ce3e41756eb
Balya Rizqi <125800839+rizqi-r@users.noreply.github.com>
2024-06-28 23:16:40 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Read environment variables
-const isProduction = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: isProduction
-      ? {}
-      : {
-          "/api": {
-            target: "https://jetlajahin.up.railway.app",
-            changeOrigin: true,
-            secure: false,
-          },
-        },
+    proxy: {
+      "/api": 'https://jetlajahin.up.railway.app'
+    },
   },
 });
